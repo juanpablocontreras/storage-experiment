f8f75d9d8898d276baa83352ff6eedef72759285
juanpablocontreras <juanpablocontreras@cmail.carleton.ca>
2020-07-28 19:05:05 -0400

    added log file creation for queue times and transmitter times

--- src/request_handlers/RequestHandler.java
+++ src/request_handlers/RequestHandler.java
@@ -3,20 +3,25 @@ package request_handlers;
 import request_types.*;
 import request_transmitters.*;
 import ioQueues.*;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Random;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
 public class RequestHandler extends Thread{
 	
 	//Fields
 	protected SyncListIOQueue ioRequestQueue; //reference to the IORequestQueue
 	protected int numIOrequestsPerDataTransfer = 1;
-	protected int interIOprocessTime = 1000;
+	protected int interIOprocessTime = 100;
 	protected int pollingTime = 100; //amount of time between unsuccessful polls
 	protected int numAttempts = 10; //number of attempts to get item from queue before proceeding to the data transfer anyway
 	protected Transmitter transmitter;
+	
+	protected String logFolderPath = "logs";
+	protected PrintWriter printQueueLog_line;
+	protected PrintWriter printTrsmLog_line;
 	
 	public RequestHandler(	
 			SyncListIOQueue requestQeue,
@@ -41,20 +46,31 @@ public class RequestHandler extends Thread{
 			//list to hold the IO requests for one data transfer
 			ArrayList<IORequest> dataTransferIORequests;
 			
+			//set up first append_to_file to false so previous files are erased
+			boolean append_to_file = false;
 			
 			while(true) {
 				//Perform 1 data transfer
 				
+				//create or set up the log files and writers
+				setUpLogPrinters(append_to_file);
+				append_to_file = true; //set to true so that the second+ data transfers print logs appended to the end of the log files
+				
 				//GET all IO requests for the data transfer into the cached list
 				dataTransferIORequests = getRequestsForTransfer();
 				
+				//log requests time spent in IO queue
+				logQueueTimes(dataTransferIORequests); 
+				
 				//Perform the IO requests of the data transfer
 				if(dataTransferIORequests != null && !dataTransferIORequests.isEmpty()) {
-					performDataTransferIORequests(dataTransferIORequests);
+					logTransmitterTimes(performDataTransferIORequests(dataTransferIORequests));
 				}else {
 					System.out.println("No Requests...");
 					Thread.sleep(pollingTime);
 				}
+				
+				closPrinters();
 			}
 		}catch(Exception e){
 			System.out.println(e);
@@ -95,28 +111,14 @@ public class RequestHandler extends Thread{
 		return dataTransferIORequests;
 	}
 	
-	private void performDataTransferIORequests(ArrayList<IORequest> dataTransferIORequests) throws Exception 
+	private ArrayList<Long> performDataTransferIORequests(ArrayList<IORequest> dataTransferIORequests) throws Exception 
 	{
 		synchronized(dataTransferIORequests) {
 			
 			if(dataTransferIORequests != null && !dataTransferIORequests.isEmpty()) {
 				
-				//output IOqueue wait times
-				
-				Iterator<IORequest> IOiter = dataTransferIORequests.iterator();
-				while(IOiter.hasNext()) {
-					IORequest tempReq = IOiter.next();
-					long queueTime = tempReq.queueTimePolled - tempReq.queueTimeArrival;
-					//System.out.println("Request ID " + tempReq.id + " Queue Arrival time: " + tempReq.queueTimeArrival);
-					//System.out.println("Request ID " + tempReq.id + " Queue Retirval time: " + tempReq.queueTimePolled);
-					System.out.println("Handler detected request ID " + tempReq.id + " spent " + queueTime + " in IO queue");
-				}
-				
-				
 				//Set up transmitter time measurements
 				ArrayList<Long> transmitterTimes = new ArrayList<Long>();
-				
-				System.out.println("---------START DATA TRANSFER-------");
 				
 				//set up connection
 				IORequest temReq = dataTransferIORequests.get(0);
@@ -125,7 +127,6 @@ public class RequestHandler extends Thread{
 				
 				//transmit the data
 				transmitterTimes.add(System.currentTimeMillis());
-				
 				for(IORequest request:dataTransferIORequests) {
 					transmitter.performIORequest(request);
 					transmitterTimes.add(System.currentTimeMillis());
@@ -134,16 +135,37 @@ public class RequestHandler extends Thread{
 				//close connection
 				transmitter.closeConnection();
 				
-				//output transmitter times
-				System.out.println("Transmitter times");
-				for(int i=1; i<transmitterTimes.size();i++) {
-					System.out.println(transmitterTimes.get(i) - transmitterTimes.get(i-1));
-				}
-				
-				System.out.println("---------END DATA TRANSFER-------");
-				
+				return transmitterTimes;
 			}
+			
+			throw new Exception("No data to transfer (dataTransferIORequests is null or empty)");
 		}
 	}
 	
+	private void setUpLogPrinters(boolean append_to_file) throws Exception {
+		FileWriter writeQ = new FileWriter(this.logFolderPath + "/queuelog",append_to_file);
+		FileWriter writeT = new FileWriter(this.logFolderPath + "/trsmlog",append_to_file);
+		
+		this.printQueueLog_line = new PrintWriter(writeQ);
+		this.printTrsmLog_line = new PrintWriter(writeT);
+	}
+	
+	private void logQueueTimes(ArrayList<IORequest> requests) {
+		for(IORequest request:requests) {
+			printQueueLog_line.print(request.queueTimePolled-request.queueTimeArrival);
+			printQueueLog_line.print(",");
+		}
+	}
+	
+	private void logTransmitterTimes(ArrayList<Long> transmitterTimes) {
+		for(int i=1;i<transmitterTimes.size();i++) {
+			printTrsmLog_line.print(transmitterTimes.get(i)-transmitterTimes.get(i-1));
+			printTrsmLog_line.print(",");
+		}
+	}
+	
+	private void closPrinters() {
+		this.printQueueLog_line.close();
+		this.printTrsmLog_line.close();
+	}
 }
